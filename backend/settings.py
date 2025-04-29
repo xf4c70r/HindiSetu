58ff172312005d2c4d4455b493905df1b94210cb
Arvind <arvindsudarshan18@gmail.com>
2025-04-29 15:18:26 -0700

    Updated stsic files settings

--- backend/settings.py
+++ backend/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
 ]
 
 # Whitenoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_USE_FINDERS = True
 WHITENOISE_MANIFEST_STRICT = False
 WHITENOISE_ALLOW_ALL_ORIGINS = True
